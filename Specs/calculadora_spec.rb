cbe558528af4a80ae40ca0dd0421f9b32398244e
Maiara Lindemaier <maiara.lindemaier@outlook.com>
2022-09-04 21:47:00 -0300

    atualização tdd  multiplicação

--- Specs/calculadora_spec.rb
+++ Specs/calculadora_spec.rb
@@ -5,7 +5,7 @@ require_relative '../src/calculadora'
 describe('Calculadora') do
     calculadora =  Calculadora.new
 
-  # adição
+    # adição
 
     it('Deve somar dois inteiros positivos') do
         expect(calculadora.soma(1, 2)).to eq 3
@@ -49,7 +49,7 @@ describe('Calculadora') do
       expect(calculadora.subtrai(10, 10)).to eq 0
     end
 
-    it('Deve subtrair 0 por qualquer numero inteiro e o resultado deve ser o numero negativo')do
+    it('Deve subtrair zero por qualquer numero inteiro e o resultado deve ser o numero negativo')do
       expect(calculadora.subtrai(0, 10)).to eq -10
     end
 
@@ -57,7 +57,11 @@ describe('Calculadora') do
       expect(calculadora.subtrai(10, 0)).to eq 10
     end
 
-    it('Deve subtrair um numero negativos e um positivo e o resultado deve ser a soma deles')do
+    it('Deve subtrair um numero negativo por zero e o resultado deve ser o mesmo numero')do
+      expect(calculadora.subtrai(-10, 0)).to eq -10
+    end
+
+    it('Deve subtrair dois numeros negativos e o resultado deve ser a soma deles')do
      expect(calculadora.subtrai(-10, 20)).to eq -30
     end
 
@@ -68,4 +72,54 @@ describe('Calculadora') do
     it('Deve subtrair dois numeros decimais com duas casas apos o .')do
       expect(calculadora.subtrai(10.55, 5.11)).to eq 5.44
     end
-end     
+  
+    #multiplicação
+  
+    it('Deve multiplicar dois numeros inteiros')do
+      expect(calculadora.multiplica(10, 5)).to eq 50
+    end
+
+    it('Deve multiplicar dois numeros iguais')do
+      expect(calculadora.multiplica(2, 2)).to eq 4
+    end  
+
+    it('Deve multiplicar dois numeros decimais')do
+      expect(calculadora.multiplica(10.5, 5.7)).to eq 59.85
+    end
+
+    it('Qualquer numero multiplicado por zero deve ser zero')do
+      expect(calculadora.multiplica(50, 0)).to eq 0
+    end
+
+    it('Deve multiplicar numeros decimais negativos e positivos')do
+      expect(calculadora.multiplica(-15, 10)).to eq -150
+     end
+    
+    it('Deve multiplicar dois numeros decimais com duas casas apos o .')do
+      expect(calculadora.multiplica(10.45, 5.20)).to eq 54.339999999999996
+    end 
+
+    it('Não deve multiplicar resultados igual a zero')do
+      expect(calculadora.multiplica(0, 0)).to eq 0 
+    end
+# divisão
+    it('Deve dividir dois numeros inteiros')do
+      expect(calculadora.dividir(8, 4)).to eq 2
+    end
+
+    it('Deve dividir numeros decimais')do
+      expect(calculadora.dividir(10.5, 2.5)).to eq 4.2
+    end
+
+    it('Deve dividir numeros decimais com duas casas apos .')do
+      expect(calculadora.dividir(10.55, 5.55)).to eq 1.900900900900901
+    end
+  
+    it('Deve dividir numeros negativos')do
+      expect(calculadora.dividir(5, -5)).to  eq -1
+    end
+    
+    it('Não calcular para a, b igual a 0')do
+      expect(calculadora.dividir(120, 0)).to include ' Não e possivel divizão por 0!' 
+    end
+  end     
--- src/calculadora.rb
+++ src/calculadora.rb
@@ -9,10 +9,14 @@ class Calculadora
   end
 
   def multiplica(a, b)
-    a * b 
+    a * b  
   end 
 
   def dividir(a, b)
+    if a == 0 || b == 0
+      return ' Não e possivel divizão por 0!'
+    end  
     a / b 
   end  
+   
 end  
